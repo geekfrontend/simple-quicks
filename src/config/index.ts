f9673fe920883b1943071344e09c9b8864105f91
geekfrontend <geekfrontend16@gmail.com>
2025-04-09 09:13:38 +0800

    fix url graphql

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  BASE_URL_GRAPHQL: "https://graphql-simple-quicks.vercel.app/",
+  BASE_URL_GRAPHQL: "https://graphql-simple-quicks.vercel.app/graphql",
   BASE_URL_API: "https://graphql-simple-quicks.vercel.app/api",
 };
